b4069f22e5185b0d4ab19c725dcbe34bc0f09f70
wolfstudy <rxl5555555@qq.com>
2018-12-12 19:25:07 +0800

    adjust code structure

--- docker/node/copernicus_exporter/main.go
+++ docker/node/copernicus_exporter/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/astaxie/beego/logs"
+	"github.com/copernet/copernicus-monitoring/docker/node/copernicus_exporter/collector"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 )
 
 func main() {
-	prometheus.MustRegister(newChainCollector())
+	prometheus.MustRegister(collector.NewChainCollector())
 
 	http.Handle("/metrics", promhttp.Handler())
 	logs.Info("Beginning to serve on port :8081")
